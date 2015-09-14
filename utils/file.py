c1cac90ff896a61fb07f6f9d40e57912077e6c3a
tianhu <tianhu@alatting.com>
2015-09-14 13:57:08 -0700

    modify utils.file to enable same filename for an instance

--- utils/file.py
+++ utils/file.py
@@ -12,13 +12,19 @@ class OverwriteStorage(FileSystemStorage):
         return super(OverwriteStorage, self).get_available_name(name, max_length)
 
 
-def get_file_path(old_filename, path, filename):
+def get_file_path(instance, field_name, path, filename):
+    old_filename = getattr(instance, '_%s_' % field_name)
     if not old_filename:
         ext = ''
         comps = filename.split('.')
         if len(comps) > 1:
             ext = comps[-1]
-        filename = "%s.%s" % (uuid.uuid4(), ext)
+        name_label = '_name_'
+        name = getattr(instance, name_label, None)
+        if name is None:
+            name = uuid.uuid4()
+            setattr(instance, name_label, name)
+        filename = "%s.%s" % (name, ext)
         path = time.strftime(path, time.gmtime())
         filename = os.path.join(path, filename)
     else:
@@ -29,35 +35,35 @@ def get_file_path(old_filename, path, filename):
 
 def get_image_path(instance, filename):
     path = 'images/%Y/%m/%d'
-    old_filename = getattr(instance, '_file_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'file'
+    return get_file_path(instance, field_name, path, filename)
 
 
 def get_video_path(instance, filename):
     path = 'videos/%Y/%m/%d'
-    old_filename = getattr(instance, '_file_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'file'
+    return get_file_path(instance, field_name, path, filename)
 
 
 def get_music_path(instance, filename):
     path = 'music/%Y/%m/%d'
-    old_filename = getattr(instance, '_file_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'file'
+    return get_file_path(instance, field_name, path, filename)
 
 
 def get_html_path(instance, filename):
     path = 'html5/%Y/%m/%d'
-    old_filename = getattr(instance, '_html_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'html'
+    return get_file_path(instance, field_name, path, filename)
 
 
 def get_css_path(instance, filename):
     path = 'html5/%Y/%m/%d'
-    old_filename = getattr(instance, '_css_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'css'
+    return get_file_path(instance, field_name, path, filename)
 
 
 def get_script_path(instance, filename):
     path = 'html5/%Y/%m/%d'
-    old_filename = getattr(instance, '_script_')
-    return get_file_path(old_filename, path, filename)
+    field_name = 'script'
+    return get_file_path(instance, field_name, path, filename)
