7a4da3cc4158ebf9b09dbff4b3872d29b24eb427
tianhu <tianhu@alatting.com>
2015-09-17 14:12:06 -0700

    add qrcode

--- alatting_website/urls.py
+++ alatting_website/urls.py
@@ -1,9 +1,10 @@
 __author__ = 'tianhuyang'
 from django.conf.urls import include, url
-from alatting_website.views import PosterView, IndexView
+from alatting_website.views import PosterView, IndexView, PosterCodeView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^poster/(?P<pk>\d+)', PosterView.as_view(), name='poster'),
+    url(r'^code/poster/(?P<pk>\d+)', PosterCodeView.as_view(), name='code_poster'),
 ]
