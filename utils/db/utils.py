7d39a7fe09a3c16786eedddc2c3f6e02f8fe8bce
tianhu <tianhu@alatting.com>
2015-09-18 16:43:38 -0700

    continue /api/auth/signup/ operation

--- utils/db/utils.py
+++ utils/db/utils.py
@@ -1,4 +1,5 @@
 __author__ = 'tianhuyang'
+import uuid
 from django.db.models import F
 
 
@@ -7,3 +8,7 @@ class Utils:
     def increase_counts(cls, queryset, field_counts):
         data = {key: F(key) + value for key, value in field_counts.items()}
         return queryset.update(**data)
+
+
+def generate_uuid():
+    return uuid.uuid4().hex
