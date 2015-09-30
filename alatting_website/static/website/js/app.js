049124ed6734e5d7356c120caea0f3bc5ec8cbab
tianhu <tianhu@alatting.com>
2015-09-29 18:46:28 -0700

    include mobileangularui framwworks

---
+++ alatting_website/static/website/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tianhuyang on 9/29/15.
+ */
+var app = angular.module('PosterApp', [
+    "ngRoute",
+    "ngTouch",
+    "mobile-angular-ui"
+]);
+
+/*app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: "signIn.html"
+    });
+});*/
