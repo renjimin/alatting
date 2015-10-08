6e16b93e614bdbc6a8668fb3e57fb2ad46e21f0e
tianhu <tianhu@alatting.com>
2015-10-07 17:55:10 -0700

    implement relative spacing and refine the header in poster view

--- utils/qrcode.py
+++ utils/qrcode.py
@@ -16,5 +16,5 @@ class QrCode:
         qr_code = pyqrcode.create(content)
         if buffer is None:
             buffer = io.BytesIO()
-        qr_code.png(buffer, scale=5)
+        qr_code.png(buffer, scale=6, quiet_zone=0, background=(0, 0, 0, 0))
         return buffer
