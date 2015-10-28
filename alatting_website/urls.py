e182ecfeb7278f51e3b59b18b2ff346e06a65af1
tianhu <tianhu@alatting.com>
2015-10-27 17:47:09 -0700

    finish comment submenu

--- alatting_website/urls.py
+++ alatting_website/urls.py
@@ -1,10 +1,11 @@
 __author__ = 'tianhuyang'
 from django.conf.urls import include, url
-from alatting_website.views import PosterView, IndexView, PosterCodeView, SvgClipView
+from alatting_website.views import PosterView, IndexView, PosterCodeView, SvgClipView, DemoView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^demo$', DemoView.as_view(), name='demo'),
     url(r'^poster/(?P<pk>\d+)/$', PosterView.as_view(), name='poster'),
     url(r'^api/qr_code/poster/(?P<pk>\d+)/$', PosterCodeView.as_view(), name='code_poster'),
     url(r'^api/svg_clip/(?P<layout_id>\d+)/(?P<shape_index>\d+).svg$', SvgClipView.as_view(), name='svg_clip'),
