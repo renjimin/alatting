c82cb4c5c0b21ac481eea46ffa90e373925295ed
direction123 <direction.wfx@gmail.com>
2016-04-13 15:00:00 -0700

    simple rss url
    further discussions and modifications are needed.

--- alatting_website/urls.py
+++ alatting_website/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import include, url
 from alatting_website.views import PosterView, IndexView, PosterCodeView, SvgClipView, DemoView, CaptureView, TestView
 from alatting_website.view.edit_view import EditView, CreatePosterView
-
+from alatting_website.feeds import PosterUpdateFeed
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^test$', TestView.as_view(), name='test'),
     url(r'^capture$', CaptureView.as_view(), name='capture'),
     url(r'^poster/(?P<pk>\d+)/$', PosterView.as_view(), name='poster'),
+    url(r'^poster/rss$', PosterUpdateFeed()),
     url(r'^poster/(?P<pk>\d+)/edit$', EditView.as_view(), name='edit'),
     url(r'^poster/create$', CreatePosterView.as_view(), name='create'),
     url(r'^api/poster/(?P<pk>\d+)/qr_code/$', PosterCodeView.as_view(),
