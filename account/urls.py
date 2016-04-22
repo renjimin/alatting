93026d18ed3d51129eb7af8ececa974749e8a102
lyhapple <liyinhuiapple@163.com>
2016-04-22 17:02:45 +0800

    login logout auth backend

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,8 @@
 
 from django.conf.urls import url
 from account.views import LoginView
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
 
 
 urlpatterns = [
-    url(r'^login$', obtain_jwt_token),
-    url(r'^token-verify$', verify_jwt_token)
+    url(r'^login$', LoginView.as_view(), name='login'),
 ]
