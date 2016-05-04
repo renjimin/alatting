ed66ce861f4f09de92f08462108f48b79eee41aa
gudlike <747531174@qq.com>
2016-05-04 11:40:09 +0800

    重置密码接口

--- account/form/forms.py
+++ account/form/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 
 
+def pwd_validate(p1, p2):
+    return p1 == p2
+
+
 class RegisterForm(forms.Form):
     username = forms.CharField(label='用户名')
     message = forms.CharField(label='验证码')
     password1 = forms.CharField(label='密码', widget=forms.PasswordInput)
     password2 = forms.CharField(label='密码确认', widget=forms.PasswordInput)
 
-    def pwd_validate(self, p1, p2):
-        return p1 == p2
+
+class ResetPasswordForm(forms.Form):
+    username = forms.CharField(label='用户名')
+    password1 = forms.CharField(label='密码', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='密码确认', widget=forms.PasswordInput)
