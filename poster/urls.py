87dd33433ec0b085e683c1c63476870569d01886
gudlike <747531174@qq.com>
2016-05-04 15:04:02 +0800

    模板选择接口

--- poster/urls.py
+++ poster/urls.py
@@ -6,8 +6,7 @@ from poster.view.poster import PosterListView, PosterSimpleInfoListView, \
 from poster.view.resource import (
     CategoryListView, CategoryKeywordListView, TemplateListView,
     UploadFileView)
-from poster.views import KeywordsView, CreateFormView
-
+from poster.views import KeywordsView, CreateFormView, SelectTemplateView
 
 urlpatterns = [
     url(r'^keywords/$', KeywordsView.as_view(),
@@ -16,7 +15,7 @@ urlpatterns = [
     url(r'^create-form/$', CreateFormView.as_view(),
         name='create_form'),
 
-    url(r'^select-template/$', CategoryListView.as_view(),
+    url(r'^select-template/$', SelectTemplateView.as_view(),
         name='select_template'),
 ]
 
--- poster/views.py
+++ poster/views.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from alatting_website.models import Template
 
 
 class KeywordsView(TemplateView):
@@ -10,5 +12,10 @@ class CreateFormView(TemplateView):
     template_name = 'poster/create-form.html'
 
 
-class SelectTemplateView(TemplateView):
+class SelectTemplateView(ListView):
+    model = Template
     template_name = 'poster/select-template.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SelectTemplateView, self).get_context_data(**kwargs)
+        return context
