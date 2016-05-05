ae5900b1de96b315012b9a560fa02d251292dcc0
tuduanfeng <1490649846@qq.com>
2016-05-05 02:03:51 -0700

    update css

--- poster/static/poster/js/basic-info.js
+++ poster/static/poster/js/basic-info.js
@@ -83,7 +83,7 @@ function submitinfo(){
 
 
 function back(){
-	yyConfirm("确定返回上一页",function(){
+	yyConfirm("当前输入信息会丢失",function(){
 		history.back(-1);
 	});
 }
@@ -101,14 +101,14 @@ $(function(){
  var oFile = this.files[0];
 
  if (oFile.size / 1024 < 100) {
-  _alertMsg.innerHTML="<font style='color:blue'>√</font>";
+  _alertMsg.innerHTML="<font style='color:blue'></font>";
  };
  if (result < 0) {
   //_alertMsg.innerHTML="请输入正确格式:" + upLoadType;
 	 yyAlert("请输入正确格式:" + upLoadType);
 
  } else{
-  _alertMsg.innerHTML="<font style='color:blue'>√</font>";
+  _alertMsg.innerHTML="<font style='color:blue'></font>";
  };
 
  oFReader.readAsDataURL(oFile);
