893a47672d161c4e5244f4ed433a61c31a7df1da
direction123 <direction.wfx@gmail.com>
2016-05-05 11:57:34 -0700

    account/profile 界面显示调整

---
+++ account/static/account/js/profile.js
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+$(document).ready(function () {
+	$(".ac-add .ac-list-item-ctrl").click(function(){
+	    alert("添加更多模板");
+	});
+	$(".ac-item-back").click(function(){
+    	alert("删除");
+	});
+	$(".ac-item-edit").click(function(){
+	    alert("编辑");
+	});
+
+});
+
