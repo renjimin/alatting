2723d6a4404125dfb6d9f17845ddfbcca192db6f
tucy9099 <tucy9099@163.com>
2016-05-11 18:01:18 +0800

    修复yyAlert、yyConfirm添加按钮文字修改接口

--- alatting_website/static/website/js/base.js
+++ alatting_website/static/website/js/base.js
@@ -1,5 +1,10 @@
 (function () {
-	function yyAlert(text,callback){
+	function yyAlert(text,callback,params){
+        var defaults = {
+            'okText':'知道了'
+        }
+        var options = extend(defaults,params);
+
         var alertEle = document.getElementById('yyalert-dialog'), coverbox,popbox,yyBtnConfirm;
 
 		if(alertEle == undefined || alertEle.length <= 0){
@@ -13,7 +18,11 @@
         coverbox = document.getElementById('yyalert-cover');
         popbox = document.getElementById('yyalert');
         yyBtnConfirm = document.getElementById('yyBtnAlert');
+
+        /* set */
+        yyBtnConfirm.innerHTML = options.okText;
         popbox.children[0].innerHTML = text;
+        /* position */
         popbox.style.marginTop = -popbox.offsetHeight/2+'px';
         popbox.style.marginLeft =  -popbox.offsetWidth/2+'px';
         setTimeout(function(){
@@ -32,7 +41,14 @@
 	window.yyAlert = yyAlert;
 })();
 (function () {
-	function yyConfirm(text,callback){
+	function yyConfirm(text,callback,params){
+		var defaults = {
+            'okText':'知道了',
+			'cancelText':'取消',
+			'cancelFun':null
+        }
+		var options = extend(defaults,params);
+
         var confirmEle = document.getElementById('yyconfirm-dialog'), coverbox, popbox, yyBtnConfirm,yyBtnCancel;
         if(confirmEle == undefined || confirmEle.length <= 0){
             var cover = '<div id="yyconfirm-cover" class="yyalert-cover" style=""><div class="yyalert-cover-inner"></div></div>';
@@ -46,6 +62,9 @@
         popbox = document.getElementById('yyconfirm');
         yyBtnConfirm = document.getElementById('yyBtnConfirm');
         yyBtnCancel = document.getElementById('yyBtnCancel');
+		/* set */
+		yyBtnConfirm.innerHTML = options.okText;
+		yyBtnCancel.innerHTML = options.cancelText;
         popbox.children[0].innerHTML = text;
 
         popbox.style.marginTop = -popbox.offsetHeight/2+'px';
@@ -57,6 +76,9 @@
         }
         yyBtnCancel.onclick=function(){
             confirmEle.classList.remove('open');
+			if(options.cancelFun!=null && typeof(eval(options.cancelFun)) == "function"){
+				options.cancelFun();
+			}
         }
         yyBtnConfirm.onclick=function(){
             confirmEle.classList.remove('open');
@@ -66,7 +88,6 @@
         }
 
 	}
-
 	window.yyConfirm = yyConfirm;
 })();
 (function () {
@@ -92,3 +113,8 @@
 	}
 	window.yyMessage = yyMessage;
 })();
+function extend(destination, source) {
+    for (var property in source)
+    destination[property] = source[property];
+    return destination;
+}
