8e12bb92f90b3a5d7ca189dd386d6333adb98d75
direction123 <direction.wfx@gmail.com>
2016-05-11 19:40:52 -0700

    /survey: jquery validation

--- survey/static/survey/js/index.js
+++ survey/static/survey/js/index.js
@@ -9,10 +9,13 @@ $(document).ready(function(){
 	// Next button click action
 	$(".next").click(function(){
 		if(current < length){
-			$(".step").addClass("hidden");
-			current = current+1;
-			$(".step:eq("+(current-1)+")").removeClass("hidden")
-			setProgress(current, length);
+			//check validation
+			if($(".form").valid()) {
+				$(".step").addClass("hidden");
+				current = current+1;
+				$(".step:eq("+(current-1)+")").removeClass("hidden")
+				setProgress(current, length);
+			}
 		}
 	});
 
@@ -23,6 +26,33 @@ $(document).ready(function(){
 			current = current-1;
 			$(".step:eq("+(current-1)+")").removeClass("hidden")
 			setProgress(current, length);
+		}
+	});
+
+	//validation rules
+	$(".form").validate({
+		rules: {
+			step1radio: {required: true},
+			step2radio: {required: true},
+			step3text: "required",
+			'step3checkbox1[]': {required: true},
+			'step3checkbox2[]': {required: true},
+			step4radio1: {required: true},
+			step4radio2: {required: true},
+			step5text: "required"
+		},
+		messages: {
+			step1radio: {required: "请选择其中一项"},
+			step2radio: {required: "请选择其中一项"},
+			step3text: {required: "请填写上课次数"},
+			'step3checkbox1[]': {required: "请至少选择其中一项"},
+			'step3checkbox2[]': {required: "请至少选择其中一项"},
+			step4radio1: {required: "请选择其中一项"},
+			step4radio2: {required: "请选择其中一项"},
+			step5text: {required: "年龄"}
+		},
+		errorPlacement: function(error, element) {
+			error.appendTo(element.parents('.qs-list-section'));
 		}
 	});
 })
