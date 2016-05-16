7a50fa13f52b26af6b88b140b08a5e71b86f56ba
direction123 <direction.wfx@gmail.com>
2016-05-16 11:49:49 -0700

    survey: start view

--- survey/urls.py
+++ survey/urls.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
 from django.conf.urls import url
-from survey.views import IndexView
+from survey.views import IndexView, StartView, QuestionnaireView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^start/(?P<user_id>\d+)/(?P<questionnaire_id>\d+)/$', StartView.as_view(), name='start'),
+    url(r'^questionnaire/', QuestionnaireView.as_view(), name='questionnaire'),
 ]
--- survey/views.py
+++ survey/views.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, RedirectView
+from survey.models import *
+
 
 class IndexView(TemplateView):
 	template_name = 'survey/survey-base.html'
@@ -9,3 +12,15 @@ class IndexView(TemplateView):
 		context = super(IndexView, self).get_context_data(**kwargs)
 		return context
 
+class StartView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('survey:questionnaire')
+
+class QuestionnaireView(TemplateView):
+	template_name = 'questionset.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(QuestionnaireView, self).get_context_data(**kwargs)
+		return context
+		
