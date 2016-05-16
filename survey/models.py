04f730af1ed0f98c141ec0e54ed6d57876856d65
direction123 <direction.wfx@gmail.com>
2016-05-15 23:48:04 -0700

    survey models

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from alatting_website.models import Category
 import os
 from django.conf import settings
+from django.contrib.auth.models import User
+from survey import QuestionChoices
 
 class Survey(models.Model):
 	name = models.CharField(max_length=64, unique=True)
@@ -21,30 +23,148 @@ class Survey(models.Model):
 	def __str__(self):
 		return "{:s}".format(self.name)
 
-	def get_template_static_dir_path(self, with_root=False):
-		dir_root = os.path.join(settings.BASE_DIR, 'survey',
-								'templates', 'survey_template')
-		full_path = os.path.join(dir_root, self.name)
-		if with_root:
-			return full_path
-		else:
-			return full_path.replace(settings.BASE_DIR, '')
 
-	def html_path(self):
-		return os.path.join(self.get_template_static_dir_path(), 'index.html')
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=128)
 
-	def css_path(self):
-		return os.path.join(self.get_template_static_dir_path(), 'index.css')
+    def __unicode__(self):
+        return "{:s}".format(self.name)
 
-	def js_path(self):
-		return os.path.join(self.get_template_static_dir_path(), 'index.js')
+    def questionsets(self):
+        if not hasattr(self, "__qscache"):
+            self.__qscache = \
+              QuestionSet.objects.filter(questionnaire=self).order_by('sortid')
+        return self.__qscache
 
-	def static_file_exists(self):
-		html = os.path.join(settings.BASE_DIR, self.html_path()[1:])
-		css = os.path.join(settings.BASE_DIR, self.css_path()[1:])
-		js = os.path.join(settings.BASE_DIR, self.js_path()[1:])
-		return all([
-			os.path.exists(html),
-			os.path.exists(css),
-			os.path.exists(js)
-		])
+    def questions(self):
+        questions = []
+        for questionset in self.questionsets():
+            questions += questionset.questions()
+        return questions
+
+class QuestionSet(models.Model):
+    "Which questions to display on a question page"
+    questionnaire = models.ForeignKey(Questionnaire)
+    sortid = models.IntegerField() # used to decide which order to display in
+    heading = models.CharField(max_length=64)
+   
+    def questions(self):
+        if not hasattr(self, "__qcache"):
+            self.__qcache = \
+            	questions_ = Question.objects.filter(questionset=self.id).order_by('sortid')
+        return self.__qcache
+
+    def next(self):
+        qs = self.questionnaire.questionsets()
+        retnext = False
+        for q in qs:
+            if retnext:
+                return q
+            if q == self:
+                retnext = True
+        return None
+
+    def prev(self):
+        qs = self.questionnaire.questionsets()
+        last = None
+        for q in qs:
+            if q == self:
+                return last
+            last = q
+
+    def is_last(self):
+        try:
+            return self.questionnaire.questionsets()[-1] == self
+        except NameError:
+            # should only occur if not yet saved
+            return True
+
+    def is_first(self):
+        try:
+            return self.questionnaire.questionsets()[0] == self
+        except NameError:
+            # should only occur if not yet saved
+            return True
+
+    def __unicode__(self):
+        return u'(%s) %d. %s' % (self.questionnaire.name, self.sortid, self.heading)
+
+
+class Question(models.Model):
+    questionset = models.ForeignKey(QuestionSet)
+    sortid = models.IntegerField()
+    text = models.TextField(blank=True, verbose_name="Text")
+    type = models.CharField(u"Type of question", max_length=32,
+        choices = QuestionChoices,
+        help_text = u"Determines the means of answering the question. " \
+        "An open question gives the user a single-line textfield, " \
+        "multiple-choice gives the user a number of choices he/she can " \
+        "choose from. If a question is multiple-choice, enter the choices " \
+        "this user can choose from below'.")
+
+    def questionnaire(self):
+        return self.questionset.questionnaire
+
+    def __unicode__(self):
+        return u'%s %s' % (unicode(self.questionset), self.text)
+
+    def choices(self):
+        res = Choice.objects.filter(question=self).order_by('sortid')
+        return res
+
+    def get_type(self):
+        "Get the type name"
+        t = self.type
+        return t
+
+    def questioninclude(self):
+        return "questionnaire/" + self.get_type() + ".html"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    sortid = models.IntegerField()
+    value = models.CharField(u"Short Value", max_length=64)
+    text = models.CharField(u"Choice Text", max_length=200)
+
+    def __unicode__(self):
+        return u'(%s) %d. %s' % (self.question.number, self.sortid, self.text)
+
+class RunInfo(models.Model):
+    "Store the active/waiting questionnaire runs here"
+    subject = models.ForeignKey(User)
+    questionset = models.ForeignKey(QuestionSet, blank=True, null=True) # or straight int?
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, **kwargs):
+        self.random = (self.random or '').lower()
+        super(RunInfo, self).save(**kwargs)
+
+    def __unicode__(self):
+        return "%s" % (self.random)
+
+    class Meta:
+        verbose_name_plural = 'Run Info'
+
+
+class Answer(models.Model):
+    subject = models.ForeignKey(User, help_text = u'The user who supplied this answer')
+    question = models.ForeignKey(Question, help_text = u"The question that this is an answer to")
+    answer = models.TextField()
+    runinfo = models.ForeignKey(RunInfo)
+
+
+class RunInfoHistory(models.Model):
+    subject = models.ForeignKey(User)
+    completed = models.DateField()
+    questionnaire = models.ForeignKey(Questionnaire)
+    runinfo = models.ForeignKey(RunInfo)
+
+    def __unicode__(self):
+        return "%s" % (self.subject, self.completed)
+
+    def answers(self):
+        "Returns the query for the answers."
+        return Answer.objects.filter(subject=self.subject, runinfo=self.runinfo)
+
+    class Meta:
+        verbose_name_plural = 'Run Info History'
--- survey/urls.py
+++ survey/urls.py
@@ -1,10 +1,8 @@
 # coding=utf-8
 
 from django.conf.urls import url
-from survey.views import IndexView, SurveyDetailView
+from survey.views import IndexView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<main_cat_pk>[\d]+)/(?P<sub_cat_pk>[\d]+)$',
-        SurveyDetailView.as_view(), name='survey_detail'),
 ]
