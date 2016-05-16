926a5a5ba9554d1667349147de6ed7c433413e15
tucy9099 <tucy9099@163.com>
2016-05-16 16:14:52 +0800

    merge

--- poster/static/poster/plugins/button/button-edit.js
+++ poster/static/poster/plugins/button/button-edit.js
@@ -275,6 +275,37 @@ var curentOpts={};
 		}
 		$('#button-model').removeClass('open')
 	}
+var editButtonBasic = function(){
+    $("#button-basic").show();
+    $("#button-border").hide();
+    if(!$('#button-model').hasClass('open')){
+        $('#button-model').addClass('open');
+        addButton();
+    }
+}
+var editButtonBorder = function(){
+    $("#button-basic").hide();
+    $("#button-border").show();
+    if(!$('#button-model').hasClass('open')){
+        $('#button-model').addClass('open');
+        addButton();
+    }
+}
+var copyButton = function(){
+    var imgclone = $('.button-element.active').clone();
+    $('.button-element').removeClass('active');
+    imgclone.animate({'top':parseInt(imgclone.css('top'))+30+'px','left':parseInt(imgclone.css('left'))+30+'px'},200);
+    fullcontainer.append(imgclone);
+    scale(imgclone);
+}
+var deleteButton = function(){
+    var imgactive = $('.button-element.active');
+
+    imgactive.animate({'width':'0','height':'0','top':parseInt(imgactive.css('top'))+imgactive.height()/2+'px','left':parseInt(imgactive.css('left'))+imgactive.width()/2+'px'},200,function(){
+        imgactive.remove();
+    });
+
+}
 
 $(function(){
     $('#systemimg-model li').click(function(){
