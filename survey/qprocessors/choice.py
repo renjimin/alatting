4b068234e30f6bd085daad8dc0f68792142e507a
direction123 <direction.wfx@gmail.com>
2016-05-19 11:35:41 -0700

    survey: 问题选项中显示文本输入框

--- survey/qprocessors/choice.py
+++ survey/qprocessors/choice.py
@@ -7,3 +7,17 @@ def question_choice(request, question):
     return {
         'choices': choices,
     }
+
+def question_choice_input(request, question):
+    choices = []
+    
+    for choice in question.choices():
+        choices.append(choice) 
+        choice.inps = []     
+        for inp in choice.inputs():
+        	choice.inps.append(inp)
+
+    return {
+        'choices': choices,
+    }
+    
