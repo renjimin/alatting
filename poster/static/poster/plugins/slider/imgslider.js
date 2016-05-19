4ecea88323c2526e3fab73b7842bd648059ea618
tucy9099 <tucy9099@163.com>
2016-05-19 17:57:28 +0800

    添加多图轮播中图片的拖动、缩放功能

--- poster/static/poster/plugins/slider/imgslider.js
+++ poster/static/poster/plugins/slider/imgslider.js
@@ -1,4 +1,119 @@
 (function ($) {
+    $.fn.imgscale = function () {
+		return this.each(function(){
+			var s = $(this);
+			var pw = s.parent().width(),
+				ph = s.parent().height(),
+				sw = s.width(),
+				sh = s.height(),
+				sl = parseInt(s.css('left')),
+				st = parseInt(s.css('top')),
+				imgW,
+				imgH;
+			if(pw/ph > sw / sh){
+                imgW = pw;
+                imgH = sh * imgW / sw;
+            }else{
+                imgW = sw * ph / sh;
+                imgH = ph;
+            }
+			s.attr({'width':imgW,'height':imgH});
+			var touchEvents={
+                'startX':0,
+                'startY':0,
+                'currentX':0,
+                'currentY':0
+            };
+			s.opt={};
+			var startDiagonal={'x':0,'y':0},endDiagonal={'x':0,'y':0};
+			s.on({
+                'touchstart':function(e){
+                    if (e.originalEvent) e = e.originalEvent;
+                    $(e.currentTarget).css({'transition':'none','position':'relative','top':'0','left':'0'});
+					$(e.currentTarget).addClass('drag-active');
+
+					s.opt.left = parseInt($(e.currentTarget).css('left'));
+					s.opt.top = parseInt($(e.currentTarget).css('top'));
+					s.opt.width = $(e.currentTarget).width();
+					s.opt.height = $(e.currentTarget).height();
+
+					var touch = e.touches[0];
+					touchEvents.startX = touch.pageX;
+                    touchEvents.startY = touch.pageY;
+					if(e.touches.length > 1){
+						var touch1 = e.touches[1];
+						startDiagonal.x = Math.abs(touch.pageX - touch1.pageX);
+						startDiagonal.y = Math.abs(touch.pageY - touch1.pageY);
+					}
+
+                },
+
+                'touchmove':function(e){
+                    if (e.originalEvent) e = e.originalEvent;
+                    e.preventDefault();e.stopPropagation();
+                    var touch = e.touches[0];
+                    touchEvents.currentX = touch.pageX;
+                    touchEvents.currentY = touch.pageY;
+
+                    var ex = touchEvents.currentX - touchEvents.startX;
+                    var ey = touchEvents.currentY - touchEvents.startY;
+
+                    var ox = s.opt.left + ex;
+                    var oy = s.opt.top + ey;
+
+					if(e.touches.length <= 1){/*单手指*/
+						//$(e.currentTarget).css({'transform': 'translate('+ox+'px,'+oy+'px)'});
+						$(e.currentTarget).css({'left': ox+'px','top':oy+'px'});
+					}else{
+						var touch1 = e.touches[1];
+						endDiagonal.x = Math.abs(touch.pageX - touch1.pageX);
+						endDiagonal.y = Math.abs(touch.pageY - touch1.pageY);
+
+						var ew = s.opt.width + parseInt(endDiagonal.x) - parseInt(startDiagonal.x),
+							eh = ew * s.opt.height / s.opt.width,
+							el = s.opt.left - ew/2,
+							et = s.opt.top - eh/2;
+						$(e.currentTarget).attr({'width':ew,'height':eh});
+
+					}
+
+                },
+                'touchend':function(e){
+                    if (e.originalEvent) e = e.originalEvent;
+                    $(e.currentTarget).removeClass('drag-active');
+                    var moveEndX = parseInt($(e.currentTarget).css('left'));
+                    var moveEndY = parseInt($(e.currentTarget).css('top'));
+                    var endX = moveEndX,endY = moveEndY,endW = $(e.currentTarget).width(),endH = $(e.currentTarget).height();
+					$('.ele3').html('moveEndY:'+moveEndY);
+					if(endW < imgW){
+						endW = imgW;
+					}
+					if(endH < imgH){
+						endH = imgH;
+					}
+                    if(parseInt(moveEndX) > 0){/*top为正 img顶部位于父元素下面*/
+                        endX = 0;
+                    }
+                    if(endW + parseInt(moveEndX) < $(e.currentTarget).parent().width()){
+                        endX = - endW + $(e.currentTarget).parent().width();
+                    }
+                    if(parseInt(moveEndY) > 0){
+                        endY = 0;
+                    }
+                    if( $(e.currentTarget).height() + parseInt(moveEndY) < $(e.currentTarget).parent().height()){
+                        endY = - endH + $(e.currentTarget).parent().height();
+						$('.ele4').html('endY:'+endY);
+                    }
+
+					$(e.currentTarget).css({'transition':'all .2s ease-in'});
+					$(e.currentTarget).css({'left': endX+'px','top': endY+'px'}).attr({'width':endW,'height':endH});
+
+
+                }
+
+			});
+		})
+	}
     $.fn.imgslider = function(opations){
         var opts = {
             'data':null
@@ -10,11 +125,10 @@
 
             $.extend(s,{'wraper':null,'swiper':null});
 
-            var imgarray  = s.find('img');
-
+            var imgarray  = s.find('.swiper-container').find('img');
 
             s.empty();
-            var $swipercon = '<div class="swiper-container"><div class="swiper-wrapper"></div></div>';
+            var $swipercon = '<div class="swiper-container"><div class="swiper-wrapper"></div><div class="swiper-pagination"></div></div>';
             s.html($swipercon);
 
             s.addImage = function(datas){
@@ -32,64 +146,9 @@
                     imgH = s.height();
                 }
                 s.wraper.append('<div class="swiper-slide"><img src="'+_option.data.file+'" width="'+imgW+'" height="'+imgH+'" ></div>');
-                s.swiper = new Swiper('.swiper-container');
-                var touchEvents={
-                    'startX':0,
-                    'startY':0,
-                    'currentX':0,
-                    'currentY':0,
-                }
-                var moveX = 0,moveY = 0;
-                s.wraper.find('img').on({
-                    'touchstart':function(e){
-                        if (e.originalEvent) e = e.originalEvent;
-                        $(e.currentTarget).css({'transition':'none'});
-                        moveX = $(e.currentTarget).attr('data-x') == undefined ? 0:$(e.currentTarget).attr('data-x');
-                        moveY = $(e.currentTarget).attr('data-y') == undefined ? 0:$(e.currentTarget).attr('data-y');
-                        var touch = e.touches[0];
-                        touchEvents.startX = touch.pageX;
-                        touchEvents.startY = touch.pageY;
-                        $(e.currentTarget).addClass('drag-active');
-                    },
-                    'touchmove':function(e){
-                        if (e.originalEvent) e = e.originalEvent;
-                        e.preventDefault();e.stopPropagation();
-                        var touch = e.touches[0];
-                        touchEvents.currentX = touch.pageX;
-                        touchEvents.currentY = touch.pageY;
+                s.swiper = new Swiper('.swiper-container',{pagination: '.swiper-pagination'});
+                s.wraper.find('img').imgscale();
 
-                        var ex = touchEvents.currentX - touchEvents.startX;
-                        var ey = touchEvents.currentY - touchEvents.startY;
-
-                        var ox = parseInt(moveX) + ex;
-                        var oy = parseInt(moveY) + ey;
-                        $(e.currentTarget).attr({'data-x':ox,'data-y':oy});
-                        $(e.currentTarget).css({'transform': 'translate('+ox+'px,'+oy+'px)'});
-
-                    },
-                    'touchend':function(e){
-                        if (e.originalEvent) e = e.originalEvent;
-                        $(e.currentTarget).removeClass('drag-active');
-                        var moveEndX = $(e.currentTarget).attr('data-x') == undefined ? 0:$(e.currentTarget).attr('data-x');
-                        var moveEndY = $(e.currentTarget).attr('data-y') == undefined ? 0:$(e.currentTarget).attr('data-y');
-                        var endX = moveEndX,endY = moveEndY;
-                        if(parseInt(moveEndX) > 0){
-                            endX = 0;
-                        }
-                        if($(e.currentTarget).width() + parseInt(moveEndX) < $(e.currentTarget).parent().width()){
-                            endX = - $(e.currentTarget).width() + $(e.currentTarget).parent().width();
-                        }
-                        if(parseInt(moveEndY) > 0){
-                            endY = 0;
-                        }
-                        if( $(e.currentTarget).height() + parseInt(moveEndY) < $(e.currentTarget).parent().height()){
-                            endY = - $(e.currentTarget).height() + $(e.currentTarget).parent().height();
-                        }
-                        $(e.currentTarget).attr({'data-x':endX,'data-y':endY});
-                        $(e.currentTarget).css({'transition':'transform .2s','transform': 'translate('+endX+'px,'+endY+'px)'});
-
-                    }
-                });
             };
             if(_option.data != null && _option.data != ''){
                 s.addImage(imgarray);
@@ -97,4 +156,5 @@
         })
 
     }
+
 })(jQuery);
