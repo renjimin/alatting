4bcbce781c05f92d43defc88f4b5f26e305b4975
lyhapple <liyinhuiapple@163.com>
2016-05-20 10:23:16 +0800

    migrate

--- poster/models.py
+++ poster/models.py
@@ -11,11 +11,11 @@ class SystemImage(models.Model):
     id = BigAutoField(primary_key=True)
     image = models.ForeignKey(Image, related_name='system_images', blank=True, null=True)
     name = models.CharField(max_length=64, unique=True)
-    # text = models.TextField(
-    #     default='',
-    #     blank=True,
-    #     help_text=u'保存svg图片格式'
-    # )
+    text = models.TextField(
+        default='',
+        blank=True,
+        help_text=u'保存svg图片格式'
+    )
 
     def __str__(self):
         return "{:d}".format(self.pk)
