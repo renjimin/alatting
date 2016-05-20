ac5ab1b003625c1bf50813f2badcb309d1ae90db
test <renjim@126.com>
2016-05-20 12:51:49 +0800

    change

--- poster/static/poster/plugins/music/music.js
+++ poster/static/poster/plugins/music/music.js
@@ -8,11 +8,13 @@
 	$('.audiolink audio').attr("src",data.file);
 	yyAlert("上传音乐成功");
 	console.log(data.file)
+	$.fn.yunyeStorage.setHead('music',data.file);
     	})
     }
     var musicbankButton = function(){
     	$(this).changeMusic({}, function (ths,img) {
 	$('.audiolink').empty().append('<audio autoplay loop="loop"></audio>');
 	$('.audiolink audio').attr("src",img);
+	console.log($.fn.yunyeStorage.setHead('music',img));
     	})
     }
