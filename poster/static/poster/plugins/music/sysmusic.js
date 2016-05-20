607c9e6b613cc04cf0752857558df03bc13a580a
test <renjim@126.com>
2016-05-20 09:36:40 +0800

    system music

--- poster/static/poster/plugins/music/sysmusic.js
+++ poster/static/poster/plugins/music/sysmusic.js
@@ -12,15 +12,14 @@
             if(!sm){
                 $.ajax({
                     type: 'GET',
-                    url: '/api/v1/poster/system/background',
+                    url: '/api/v1/poster/system/music',
                     success: function (data) {
                         console.log(data);
-                        var cbox= '<div class="system-music"><div ><i id = "okmusic" class="glyphicon glyphicon-ok"></i>'+
+                        var cbox= '<div class="system-music"><div >'+
                       '<h3>系统音乐</h3><i id = "closesmusic" class="glyphicon glyphicon-remove"></i></div><ul>';
                         for(var i=0;i<data.length;i++){
                             var sysm = data[i].thumbnail_url;
-                            cbox += '<li class="music-system" data-img="'+data[i].image_url+'"><a class = "musictext">'+""+
-                            '<span class = "musiclisten" title = "试听">试听</span></a></li>';
+                            cbox += '<li class="music-system" data-img="'+data[i].music_url+'"><a class = "musictext">'+data[i].name+'<span data-img="'+data[i].music_url+'" class = "musiclisten" title = "试听">试听</span><span data-img="'+data[i].music_url+'" class = "musicapp" title = "应用">应用</span></a></li>';
                         }
                         cbox += '</ul></div>';
                         smusic.append(cbox);
@@ -37,10 +36,17 @@
             var s  = this.options;
                 var ele = this.$element;
                 var callBack_Selected =this.callBack;
-                smusic.off('click','.music-system').on('click','.music-system',function(){
-                var sm = $(this).attr('data-img');
-                callBack_Selected(ele,img);
-            });
+                smusic.off('click','.musiclisten').on('click','.musiclisten',function(){
+                    var sm = $(this).attr('data-img');
+                    console.log(sm)    
+                    $('.audiolink').empty().append('<audio autoplay></audio>');
+                    $('.audiolink audio').attr("src",sm);
+
+                });
+                smusic.off('click','.musicapp').on('click','.musicapp',function(){
+                    var sm = $(this).attr('data-img');
+                    callBack_Selected(ele,sm);
+                })
         }
 
     //在插件中使用sysMusic对象
