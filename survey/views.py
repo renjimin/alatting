e8a75e39e7aacf9da9cb7812584060f9dfc08e1c
direction123 <direction.wfx@gmail.com>
2016-05-20 16:30:50 -0700

    survey: remove old files

--- survey/views.py
+++ survey/views.py
@@ -12,7 +12,7 @@ from survey.models import *
 from survey import *
 
 class IndexView(TemplateView):
-	template_name = 'survey/survey-base.html'
+	template_name = 'questionset.html'
 
 	def get_context_data(self, **kwargs):
 		context = super(IndexView, self).get_context_data(**kwargs)
