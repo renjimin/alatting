2efcbd67b09ed74ea8a226699c99f6ed60b5e694
tucy9099 <tucy9099@163.com>
2016-05-24 20:05:24 +0800

    modify button-edit.js

--- poster/static/poster/js/show/poster.js
+++ poster/static/poster/js/show/poster.js
@@ -2,5 +2,13 @@ $(function(){
     $('.slider-content').imgslidershow();
 
     var templateScale = $('body').width()/$('.yunye-template').width();
-    $('.yunye-template').css({'transform':'scale('+templateScale+','+templateScale+')'});
+    var templateScale = $('body').width()/$('.yunye-template').width();
+    var templateScaleOpt =
+        '-webkit-transform:scale('+templateScale+','+templateScale+');'
+       +   '-moz-transform:scale('+templateScale+','+templateScale+');'
+       +     '-o-transform:scale('+templateScale+','+templateScale+');'
+       +    '-ms-transform:scale('+templateScale+','+templateScale+');'
+       +        'transform:scale('+templateScale+','+templateScale+');'
+
+    $('.yunye-template').attr('style',templateScaleOpt);
 });
