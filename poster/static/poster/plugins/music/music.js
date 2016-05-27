3e621d9b1247863a6d2d1adc317addc07c03451b
test <renjim@126.com>
2016-05-27 08:10:11 +0800

    fix

--- poster/static/poster/plugins/music/music.js
+++ poster/static/poster/plugins/music/music.js
@@ -4,19 +4,22 @@
     }
     var musicuploadButton = function(){
     	$.fn.uploadsaudio.showDialog(function(data){
-	$('.audiolink').empty().append('<audio autoplay  id= "background_music" loop="loop"></audio>');
-	$('.audiolink audio').attr("src",data.file);
-        var bg_music = document.getElementById("background_music"); 
-        if (bg_music.paused == false) {
-            bg_music.pause();
-            $(".header-music").removeClass('rotate');
-        } else {
-            bg_music.play();
-            $(".header-music").addClass('rotate');
-       }
-	console.log(data)
-	$.fn.yunyeStorage.setHead('music',data.file);
-    	})
+    	$('.audiolink').empty().append('<audio autoplay  id= "background_music" loop="loop"></audio>');
+    	$('.audiolink audio').attr("src",data.file);
+          var bg_music = document.getElementById("background_music"); 
+          if (bg_music.paused == false) {
+              bg_music.pause();
+              $(".header-music").removeClass('rotate');
+          } else {
+              bg_music.play();
+              $(".header-music").addClass('rotate');
+         }
+  	console.log(data)
+  	$.fn.yunyeStorage.setHead('music',data.file);
+        $.fn.yyTools.mask(0);
+      	},function(data){
+
+        })
     }
     var musicbankButton = function(){
     	$(this).changeMusic({}, function (ths,img) {
