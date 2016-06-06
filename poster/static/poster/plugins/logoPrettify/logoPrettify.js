c266ecfbeb017c9ec4886c648d5d398a70ec5368
Eagune <jingdiqkl123@qq.com>
2016-06-06 15:53:11 +0800

    asdasd

--- poster/static/poster/plugins/logoPrettify/logoPrettify.js
+++ poster/static/poster/plugins/logoPrettify/logoPrettify.js
@@ -5,8 +5,6 @@ $(
 
 		this.init = function(){
 			canvas = document.getElementById("editCanvas");
-			canvas.width = 2 * $(document).width() *0.8;
-			canvas.height = 2 * ($(document).height() - 220)*0.8;
 			ctx = canvas.getContext('2d');
 			$("#logoPrettify").show();
 			self.bindEvents();
@@ -33,16 +31,26 @@ $(
 			var image = new Image();
 			image.src = url;
 			var width = image.naturalWidth,
-				height = image.naturalHeight,
-				canvasW = canvas.width,
-				canvasH = canvas.height,
-				scale = width/height,
-				canvasScale = canvasW/canvasH;
-			if(scale>canvasScale){
-				ctx.drawImage(image,0,0,width,height,0,(canvasH - canvasW/scale)/2,canvasW,canvasW/scale);
+				height = image.naturalHeight;
+			canvas.width = width;
+			canvas.height = height;
+			ctx.drawImage(image,0,0,width,height,0,0,width,height);
+			var 	scale = width/height;
+			if(scale>1){
+				$("#editCanvas").height(    ($(".body-container").width()) * 0.8    ) ;
+				$("#editCanvas").width(    $("#editCanvas").width() / scale    ) ;
 			}else{
-				ctx.drawImage(image,0,0,width,height,(canvasW - canvasH*scale)/2,0,canvasH*scale,canvasH);
+				$("#editCanvas").height(    ($(".body-container").height() - 220) * 0.8    ) ;
+				$("#editCanvas").width(    $("#editCanvas").height() * scale    ) ;
 			}
+			
+			// var 	scale = width/height,
+			// 	canvasScale = canvasW/canvasH;
+			// if(scale>canvasScale){
+			// 	ctx.drawImage(image,0,0,width,height,0,(canvasH - canvasW/scale)/2,canvasW,canvasW/scale);
+			// }else{
+			// 	ctx.drawImage(image,0,0,width,height,(canvasW - canvasH*scale)/2,0,canvasH*scale,canvasH);
+			// }
 		};
 		this.uploadImage = function(){
 
