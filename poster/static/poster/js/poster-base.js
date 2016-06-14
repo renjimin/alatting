d86c389aabf424082523466062ff7896ac8a8006
Eagune <jingdiqkl123@qq.com>
2016-06-14 17:08:38 +0800

    简述进入处理

--- poster/static/poster/js/poster-base.js
+++ poster/static/poster/js/poster-base.js
@@ -15,9 +15,18 @@ $(function () {
 		//简述
 		if(g.short_description)$("#short_description").html(g.short_description);
 		if(storageAPI.getCss("#short_description"))$("#short_description").css(storageAPI.getCss("#short_description"));
-		if(g.short_description.length>40){
+		var len,cStr = 0,eStr = 0,i ;
+		for (i = 0; i < g.short_description.length.length ; i++) {    
+			if (g.short_description.length.charCodeAt(i)>127 || g.short_description.length.charCodeAt(i)==94) {    
+				cStr++;
+			 } else {    
+				eStr++;
+			}
+			len = cStr*2 +eStr;
+		}
+		if(len>40){
 			$("#short_description").css("fontSize","12px");
-		}else if(g.short_description.length > 32 ){
+		}else if(len > 32 ){
 			$("#short_description").css("fontSize","14px");
 		}else{
 			$("#short_description").css("fontSize","16px");
