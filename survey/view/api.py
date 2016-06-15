93abfdc378094e5e5b4649feb0f48ffc3beb1a70
direction123 <direction.wfx@gmail.com>
2016-06-15 16:16:16 -0700

    survey: short text

--- survey/view/api.py
+++ survey/view/api.py
@@ -13,10 +13,18 @@ class QuestionCreateAPIView(APIView):
 		if not qs_text:
 			data = {'error':'请填写问题描述'}
 			return Response(data, status=status.HTTP_404_NOT_FOUND)
+		qs_short_text = request.data["qs_short_text"]
+		if not qs_short_text:
+			data = {'error':'请填写问题简短描述'}
+			return Response(data, status=status.HTTP_404_NOT_FOUND)
+		elif len(qs_short_text)>6:
+			data = {'error':'问题简短描述不超过6个字'}
+			return Response(data, status=status.HTTP_404_NOT_FOUND)
 		qs_type = request.data["qs_type"]
 		if not qs_type:
 			data = {'error':'请填写问题类型'}
 			return Response(data, status=status.HTTP_404_NOT_FOUND)
+
 		
 
 		poster_id = request.data["poster_id"]
@@ -35,7 +43,7 @@ class QuestionCreateAPIView(APIView):
 		q.sortid = qs.questions_count() + 1
 		q.type = qs_type
 		q.text = qs_text
-		q.short_text = "5"
+		q.short_text = qs_short_text
 		q.required = False
 		q.audit_status = 0
 		q.poster = poster
