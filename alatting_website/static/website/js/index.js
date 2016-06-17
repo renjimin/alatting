7f7666654ec5d6ac623e5e51edfd3ccfed12d9dc
Eagune <jingdiqkl123@qq.com>
2016-06-17 09:11:01 +0800

    修复bug

--- alatting_website/static/website/js/index.js
+++ alatting_website/static/website/js/index.js
@@ -50,27 +50,13 @@ $(function(){
 
 	});
 
-	$('#btn-create').click(function(){
-		var login_url = $(this).data('login');
-		if(login_url !== ""){
-			yyAlert("您需要登录才能创建海报", function () {window.location.href = login_url;});
-			return false;
-		}
-		if($('#type-model').hasClass('open')){
-			$('#type-model').removeClass('open');
-			$('#type-model').off('touchstart');
-		}else{
-			$('#type-model').addClass('open');
-		}
-	});
-
 	$('#hide-cate').click(function(){
 		$('#type-model').removeClass('open');
 	});
 
     $('#btn-create').click(function(){
         var login_url = $(this).data('login');
-        if(login_url != ""){
+        if(login_url !== ""){
             yyAlert("您需要登录才能创建海报", function () {
                 window.location.href = login_url;
             });
@@ -83,26 +69,6 @@ $(function(){
             //$('#type-model').css('top',$('body').scrollTop()+'px');
             $('#type-model').addClass('open');
             var top,startY;
-            /*$('#type-model .type-model-containar').on({
-                'touchstart':function(e){
-                    if (e.originalEvent) e = e.originalEvent;
-                    e.preventDefault();e.stopPropagation();
-                    var touch = e.touches[0];
-                    startY = touch.pageY;
-                    top = $('#type-model').find('.type-model-inner').scrollTop();
-                },
-                'touchmove':function(e){
-                    if (e.originalEvent) e = e.originalEvent;
-                    var touch = e.touches[0];
-                    var endY = touch.pageY;
-                    var st = startY - endY +top;
-                    $('#type-model').find('.type-model-inner').animate({scrollTop: st},0);
-                },
-                'touchend':function(event){
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-            });*/
         }
     });
 
